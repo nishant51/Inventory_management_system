4141dbc13ff843834f3ba57f071398b4c96a8b59
nishant <timsinanishant51@gmail.com>
2023-08-29 22:32:26 +0300

    database setup and user model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
+CATEGORY = (
+    ('ELECTRONIC','ELECTRONIC'),
+    ('STATIONARY','STATIONARY'),
+    ('HARDWARE','HARDWARE'),
+)
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50 ,null= True)
+    category = models.CharField(max_length=20, choices = CATEGORY,null=True)
+    quanitiy = models.PositiveIntegerField(null=True)
+
+    class Meta:
+        verbose_add_plural:'Product'
+
+    def __str__(self):
+        return f'{self.name} of quantity {self.quanitiy}'
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
+    staff = models.ForeignKey(User,models.CASCADE,null=True)
+    orderquantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_add_plural:'Order'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='dashboard-index'),
-    path ('staff',views.staff,name='dashborad-staff')
+    path ('staff',views.staff,name='dashboard-staff'),
+    path('product',views.product,name='dashboard-product'),
+    path('order/',views.order,name='dashboard-order'),
 ]
