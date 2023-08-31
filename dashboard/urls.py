05334c0d9a30067b345ab29210d8ebdd3ee6b680
nishant <timsinanishant51@gmail.com>
2023-08-31 22:29:09 +0300

    user login nd register completed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path ('staff',views.staff,name='dashboard-staff'),
     path('product',views.product,name='dashboard-product'),
     path('order/',views.order,name='dashboard-order'),
+    path('login/',views.login,name='user-login'),
+    path('register/',views.register,name='user-register'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib import messages
 
 def index(request):
     return render(request,'dashboard/index.html')
@@ -11,3 +13,22 @@ def product(request):
 
 def order(request):
     return render(request,'dashboard/order.html')
+
+def login(request):
+     return render(request,'user/login.html')
+
+def register(request):
+   form = createuserform()
+   if request.method == 'POST':
+       form = createuserform(request.POST)
+       if form.is_valid:
+           form.save()
+           user=form.cleaned_data.get('username')
+           messages.success(request,'Account was created sucessfully' + user)
+           return redirect('user-login')
+       else:
+           form = createuserform()
+   context = {
+        'form':form
+    }
+   return render(request,'user/register.html',context)
